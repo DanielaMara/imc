2254da8603a6edd7949440e96ad27f3a3128b675
DanielaMara <danielamaradms@gmail.com>
2015-05-12 22:01:19 -0300

    Modificacoes

--- js/IMCService.js
+++ js/IMCService.js
@@ -1,11 +1,12 @@
 var IMCService = {
-    
-    calculate: function(height, weight){
-      return  weight / (height * height);
-    },
-    
-    //TODO to implemented
-    description: function(index){
-        return "To be implemented";
-    }
+	
+	calculate: function(height, weight) {
+		return weight / (height * height);
+	},
+	
+	//TODO to implemented
+	description: function(index) {
+		return "To be implemented";
+	}
+	
 };
